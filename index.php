2999257cb571e2cd45657852e3a841961efdccbd
Duong Tung Anh <duongtunganh2111@gmail.com>
2021-02-01 19:14:34 +0700

    ok

---
+++ index.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>C4K60 - Tin tức</title>
+<!-- Bộ mã Bootstrap 4 -->
+      <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Bộ mã jQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+     <!-- Bộ mã JavaScript cho Bootstrap 4 -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <!-- Bộ mã Font Awesome -->
+      <script src="https://kit.fontawesome.com/5468db3c8c.js" crossorigin="anonymous"></script>
+</head>
+
+<body style="background-color: #f0f2f5;">
+<nav class="navbar navbar-expand-md bg-warning navbar-light">
+	<a class="navbar-brand" href="/">
+    <img src="c4k60.png" alt="Logo" style="width:40px;">
+  </a>
+  <ul class="navbar-nav mr-auto">
+    
+    <form class="form-inline" action="/action_page.php">
+    <input class="form-control mr-sm-2" type="text" placeholder="Tìm kiếm">
+    <button class="btn btn-success" type="submit" style="background-color:#e4562b85;border-color:#e4562b85"><i class="fas fa-search"></i></button>
+  </form>
+    
+  </ul>
+   <ul class="navbar-nav">
+  
+  <li class="nav-item">
+      <a class="nav-link" href="#" style="font-size: 1.4rem;"><i class="fas fa-user-plus"></i></a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#" style="font-size: 1.4rem;"><i class="fas fa-comment"></i></a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#" style="font-size: 1.4rem;"><i class="fas fa-globe-americas"></i></a>
+    </li>
+  <a class="navbar-brand" href="/">
+    <img src="tunna.jpg" alt="Logo" style="width:40px;border-radius: 50%;margin-left: 10px;">
+  </a>
+</ul>
+</nav>
+<style type="text/css">
+	.content{
+	width: 600px;
+    margin: 0 auto;
+    }
+    #mydiv{
+    	    box-shadow: 0px 0px 8px -1px rgb(0 0 0 / 75%);
+    -webkit-box-shadow: 0px 0px 3px -1px rgb(0 0 0 / 75%);
+    -moz-box-shadow: 0px 0px 8px -1px rgba(0,0,0,0.75);
+    border-radius: 7px;
+    }
+</style>
+<div class="content">
+<div id="mydiv" style="background-color: white;padding-top: 10px;padding-left:20px;padding-right: 20px;padding-bottom: 48px;margin-top: 20px;">
+  <div>
+<img src="/tunna.jpg" style="border-radius: 50%;width: 40px;display: inline-block;">
+
+<div id="textbox" onclick="setTimeout(myFunction2, 2000);" data-toggle="modal" data-target="#myModal" style="display: inline-block;;width: 510px;margin-left: 5px;height: 41px;padding-top: 8px;border-radius: 20px;padding-left: 13px;transition: 0.3s;cursor: pointer;">Bạn đang nghĩ gì vậy, Tùng Anh?</div>
+<style type="text/css">
+  #textbox{
+    background-color: #f0f2f5
+
+   }
+  #textbox:hover{
+
+    background-color: #e6e6e6
+   }
+</style>
+<hr style="margin-top: 10px;margin-bottom: 8px;">
+</div>
+<div id="butto1" style="
+    
+    height: 40px;
+    width: 270px;
+    padding-top: 8px;
+    position: absolute;
+    float: left;
+    border-radius: 7px;transition: 0.3s;
+cursor: pointer;
+">
+<div style="width: auto;
+  padding: 0 86px;
+  height: 100%;
+  width: 280px;
+  float: left;
+  text-align: left;
+ color: #65676b;">
+ 
+<i class="fas fa-images" style="color: #45bd62"></i> Ảnh/Video
+  </div>
+</div>
+<div id="butto2" style="
+     height: 40px;
+    width: 270px;
+    padding-top: 8px;
+ border-radius: 7px;
+    float: right;transition: 0.3s;cursor: pointer;
+">
+  <div style="   width: auto;
+  padding: 0 51px;
+  height: 100%;
+  float: right;
+  text-align: left;
+ color: #65676b">
+<i class="fas fa-smile" style="color: #f7b928"></i> Cảm xúc/Hoạt động
+</div>
+  </div>
+  <style type="text/css">
+   #butto1:hover{
+    background-color: #f0f2f5
+   }
+   #butto2:hover{
+    background-color: #f0f2f5
+   }
+
+ </style>
+</div>
+
+</div>
+</body>
+
+</html>
